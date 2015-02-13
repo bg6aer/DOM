8ed425c13b5ce583d2c77d6ae5b71bb01df88d24
Ejz <ejz@ya.ru>
2015-02-13 18:09:27 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+PREFIX=${1:-/usr/local}
+BIN="$PREFIX/bin"
+
+if [ ! -d "$PREFIX" ]; then
+    echo "Directory $PREFIX does not exist!"
+    exit
+fi
+
+SOURCE=`pwd`
+DESTINATION="$PREFIX/cli-dom"
+
+echo "Install $SOURCE to $DESTINATION ... [OK]"
+
+if [ -d "$DESTINATION" ]; then
+    echo "Destination ($DESTINATION) already exists!"
+    exit
+fi
+
+cp -r "$SOURCE" "$DESTINATION"
+ln -s "$DESTINATION/cli-dom" "$BIN/cli-dom"
+
+echo "Finalize ... [OK]"
+
+chmod -R 755 "$DESTINATION/cli-dom"
+chmod 755 "$BIN/cli-dom"
